0c6519b0318ab0c1f6cda3aa2a34443955b63298
EliiShh <eliyahushmul@gmail.com>
2025-02-07 01:56:46 +0200

    added Logger for messegs with singelton

---
+++ AirForcesDesignPatterns/LoggerSingelton/Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirForcesDesignPatterns.LoggerSingelton
+{
+    public sealed class Logger
+    {
+        private static Logger instance = null;
+        private static readonly object padlock = new object();
+
+        private Logger() { }
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Logger();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log: {message}");
+        }
+    }
+}
