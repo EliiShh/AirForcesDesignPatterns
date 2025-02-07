cb17e3f0a3bf6119883fabff639540c07c240a41
EliiShh <eliyahushmul@gmail.com>
2025-02-07 03:50:15 +0200

    added AirForceControl class use in ISubject interface

---
+++ AirForcesDesignPatterns/BaseStationObserver/AirForceControl.cs
@@ -0,0 +1,38 @@
+﻿using AirForcesDesignPatterns.LoggerSingelton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirForcesDesignPatterns.BaseStationObserver
+{
+    public class AirForceControl : ISubject
+    {
+        private List<IObserver> observers = new List<IObserver>();
+
+        public void Attach(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void Notify(string message)
+        {
+            foreach (IObserver observer in observers)
+            {
+                observer.Update(message);
+            }
+        }
+
+        public void ChangeAircraftStatus(string status)
+        {
+            Logger.Instance.Log($"Aircraft status changed to: {status}");
+            Notify(status);
+        }
+    }
+}
