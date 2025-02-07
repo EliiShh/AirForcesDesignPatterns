4618187bbe5d2420b60b0e3b70f9728321b98ca5
EliiShh <eliyahushmul@gmail.com>
2025-02-07 02:53:43 +0200

    added GeneralAircraftFactory to create an aircraft

---
+++ AirForcesDesignPatterns/AircraftFactoryMethod/GeneralAircraftFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirForcesDesignPatterns.AircraftFactoryMethod
+{
+    public class GeneralAircraftFactory : AircraftFactory
+    {
+        public override IAircraft CreateAircraft(AircraftType type)
+        {
+            switch (type)
+            {
+                case AircraftType.FighterJet:
+                    return new FighterJet();
+                case AircraftType.TransportPlane:
+                    return new TransportPlane();
+                default:
+                    throw new ArgumentException("Invalid aircraft type");
+            }
+        }
+    }
+}
+
