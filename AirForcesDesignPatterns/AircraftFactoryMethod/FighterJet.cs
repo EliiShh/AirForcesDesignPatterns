6df7f1fe2fd8229903de5acac26fe25d334a8ace
EliiShh <eliyahushmul@gmail.com>
2025-02-07 02:41:33 +0200

    added Aircrafts used in IAircraft with the fly method

---
+++ AirForcesDesignPatterns/AircraftFactoryMethod/FighterJet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirForcesDesignPatterns.LoggerSingelton;
+
+namespace AirForcesDesignPatterns.AircraftFactoryMethod
+{
+    public class FighterJet : IAircraft
+    {
+        public void Fly()
+        {
+            Logger.Instance.Log("Fighter Jet is flying.");
+        }
+    }
+}
---
+++ AirForcesDesignPatterns/AircraftFactoryMethod/TransportPlane.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirForcesDesignPatterns.LoggerSingelton;
+
+
+namespace AirForcesDesignPatterns.AircraftFactoryMethod
+{
+    public class TransportPlane : IAircraft
+    {
+        public void Fly()
+        {
+            Logger.Instance.Log("Transport Plane is flying.");
+        }
+    }
+}
