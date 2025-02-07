876400aca71d8cd0289c851f8b494a98ad100ff4
EliiShh <eliyahushmul@gmail.com>
2025-02-07 04:11:12 +0200

    I finished the project

--- AirForcesDesignPatterns/Program.cs
+++ AirForcesDesignPatterns/Program.cs
@@ -3,41 +3,25 @@ using AirForcesDesignPatterns.AircraftFactoryMethod;
 using AirForcesDesignPatterns.BaseStationObserver;
 
 
-//public class Program
-//{
-//    static void Main(string[] args)
-//    {
-//        AircraftFactory aircraftFactory = new GeneralAircraftFactory();
+public class Program
+{
+    static void Main(string[] args)
+    {
+        AircraftFactory aircraftFactory = new GeneralAircraftFactory();
 
-//        IAircraft fighterJet = aircraftFactory.CreateAircraft(AircraftType.FighterJet);
-//        fighterJet.Fly();
+        IAircraft fighterJet = aircraftFactory.CreateAircraft(AircraftType.FighterJet);
+        fighterJet.Fly();
 
-//        IAircraft transportPlane = aircraftFactory.CreateAircraft(AircraftType.TransportPlane);
-//        transportPlane.Fly();
+        IAircraft transportPlane = aircraftFactory.CreateAircraft(AircraftType.TransportPlane);
+        transportPlane.Fly();
 
-//        AirForceControl control = new AirForceControl();
-//        BaseStation base1 = new BaseStation("Base 1");
-//        BaseStation base2 = new BaseStation("Base 2");
+        AirForceControl control = new AirForceControl();
+        BaseStation base1 = new BaseStation("Base 1");
+        BaseStation base2 = new BaseStation("Base 2");
 
-//        control.Attach(base1);
-//        control.Attach(base2);
+        control.Attach(base1);
+        control.Attach(base2);
 
-//        control.ChangeAircraftStatus("Blaaaaa");
-//    }
-//}
-AircraftFactory aircraftFactory = new GeneralAircraftFactory();
-
-IAircraft fighterJet = aircraftFactory.CreateAircraft(AircraftType.FighterJet);
-fighterJet.Fly();
-
-IAircraft transportPlane = aircraftFactory.CreateAircraft(AircraftType.TransportPlane);
-transportPlane.Fly();
-
-AirForceControl control = new AirForceControl();
-BaseStation base1 = new BaseStation("Base 1");
-BaseStation base2 = new BaseStation("Base 2");
-
-control.Attach(base1);
-control.Attach(base2);
-
-control.ChangeAircraftStatus("Blaaaaa");
+        control.ChangeAircraftStatus("Blaaaaa");
+    }
+}
