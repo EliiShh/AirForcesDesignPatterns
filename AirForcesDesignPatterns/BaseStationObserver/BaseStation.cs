3311e152b219d2dd6c9b868b59063dd7cd49e727
EliiShh <eliyahushmul@gmail.com>
2025-02-07 03:45:41 +0200

    added BaseStation class use in IObserver interface

---
+++ AirForcesDesignPatterns/BaseStationObserver/BaseStation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirForcesDesignPatterns.BaseStationObserver
+{
+    public class BaseStation : IObserver
+    {
+        private string name;
+
+        public BaseStation(string name)
+        {
+            this.name = name;
+        }
+
+        public void Update(string message)
+        {
+            Console.WriteLine($"{name} received update: {message}");
+        }
+    }
+}
